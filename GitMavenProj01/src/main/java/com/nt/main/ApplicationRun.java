570729d34a4da1045e92f563984e56ce3ed9d5f5
dassa <dassa@LAPTOP-7JV3JEFL>
2024-07-20 16:35:43 +0530

    Dev-1 Arithmetic Operations

--- GitMavenProj01/src/main/java/com/nt/main/ApplicationRun.java
+++ GitMavenProj01/src/main/java/com/nt/main/ApplicationRun.java
@@ -1,7 +1,10 @@
 package com.nt.main;
 
+import com.nt.service.Arithmetic;
+
 public class ApplicationRun {
       public static void main(String[] args) {
-		
+		Arithmetic ar = new Arithmetic();
+		System.out.println("Sum is:: "+ar.sum(10, 10));
 	}
 }
--- GitMavenProj01/src/main/java/com/nt/service/Arithmetic.java
+++ GitMavenProj01/src/main/java/com/nt/service/Arithmetic.java
@@ -1,5 +1,7 @@
 package com.nt.service;
 
 public class Arithmetic {
-
+    public int sum(int x, int y) {
+    	return x+y;
+    }
 }
