fd5891010927cf516fd398f70e26ac757fce8651
dassa <dassa@LAPTOP-7JV3JEFL>
2024-07-21 20:38:44 +0530

    TL Netbanking Improvisation

--- GitMavenProj01/src/main/java/com/nt/service/NetBanking.java
+++ GitMavenProj01/src/main/java/com/nt/service/NetBanking.java
@@ -1,7 +1,7 @@
 package com.nt.service;
 
 public class NetBanking {
-    public String netBanking() {
+    public String netBanking(String uname, String pass) {
     	return "NetBanking is added";
     }
 }
